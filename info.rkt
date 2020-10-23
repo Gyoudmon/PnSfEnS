26a10a80c95f57a0cd2ecfe9d7535ead86260303
WarGrey Gyoudmon Ju <juzhenliang@gmail.com>
2020-10-23 11:36:14 +0800

    transferred to Gyoudmon

--- info.rkt
+++ info.rkt
@@ -1,11 +1,13 @@
 #lang info
 
-(define collection 'multi)
-(define pkg-desc "Self Teaching Projects")
+(define collection 'use-pkg-name)
+(define pkg-desc "Probability and Statistics for Engineering and the Science")
 
-(define deps '("digimon" "base" "math-lib" "plot-gui-lib" "plot-lib" "scribble-lib" "typed-racket-lib"))
-(define build-deps '("digimon" "math-lib" "typed-racket-lib" "racket-doc" "math-doc" "plot-doc"))
+(define deps '("digimon"))
+(define build-deps '("digimon" "scribble-lib" "racket-doc"))
 
 (define version "1.0")
 (define pkg-authors '(wargrey))
 (define test-omit-paths 'all)
+
+(define scribblings '(["tamer/PnSfEnS.scrbl" (main-doc multi-page) ("Gyoudmon")]))
